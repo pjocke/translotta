2a22e69e336854020a782c81ad5416ddb3d5d45e
Joakim Bomelin <joakim@bomelin.com>
2017-11-09 15:45:44 +0100

    Added handling of empty reutrn value from StructSearch().

--- structsearch_test.go
+++ structsearch_test.go
@@ -8,6 +8,10 @@ func TestStructSearch(t *testing.T) {
 	want := [3]string{"scream", "fearful", "cold_sweat"}
 	have := StructSearch("frightened")
 
+	if len(have) == 0 {
+		t.Error("StructSearch() returned nothing.")
+	}
+
 	for i := range have {
 		if have[i] != want[i] {
 			t.Errorf("Want: %s, have: %s\n", want[i], have[i])
